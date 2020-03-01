2089e902aa78f55a80738915b9cdebc5215c989f
Sven Njegac <sven.njegac@gmail.com>
2020-03-01 22:41:36 +0100

    chore: separate name for service, deployment, selector and container

--- client/kubernetes/client_logs.sh
+++ client/kubernetes/client_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # logs for each of the client pods
-kubectl get pods --selector=app=ip-service-client | grep ip-service-client | while read line
+kubectl get pods --selector=app=ip-service-client-pod-selector | grep ip-service-client | while read line
 do
   POD="$(cut -d' ' -f1 <<< $line)"
   echo "Pod: $POD"
--- server/kubernetes/server_logs.sh
+++ server/kubernetes/server_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # logs for each of the server pods
-kubectl get pods --selector=app=ip-service-server | grep ip-service-server | while read line
+kubectl get pods --selector=app=ip-service-server-pod-selector | grep ip-service-server | while read line
 do
   POD="$(cut -d' ' -f1 <<< $line)"
   echo "Pod: $POD"
